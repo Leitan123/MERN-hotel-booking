586a201f6b70bcc8fb6d656c58b5ecbcff6aff4d
Thimothy <leitanthimothy@gmail.com>
2024-07-25 18:00:53 +0530

    created eroor handling to the code

--- backend/routes/hotels.js
+++ backend/routes/hotels.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
 
 const router = express.Router();
 
@@ -53,12 +54,15 @@ router.get("/:id", async (req, res) => {
 });
 
 //GET ALL
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
+  const failed = true;
+
+  if (failed) return next(createError(401, "You are not authenticated"));
   try {
     const Hotels = await Hotel.find(req.params.id);
     res.status(200).json(Hotels);
   } catch (err) {
-    res.status(500).json(err);
+    next(err);
   }
 });
 
